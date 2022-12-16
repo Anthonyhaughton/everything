2fdadb4602d2e4b090a895b9f3dd823e5d5b0c06
Haughton <ahaughton@corp.arete.com>
2022-12-16 16:07:58 -0500

    slot fixes

--- My Projects/slot_machine.py
+++ My Projects/slot_machine.py
@@ -38,7 +38,7 @@ def check_winnings(columns, lines, bet, values):
                 break
         else:
             winnings += values[symbol] * bet
-            winning_lines.append(lines + 1)
+            winning_lines.append(line + 1)
             
     return winnings, winning_lines
                 
@@ -152,7 +152,7 @@ def spin(balance):
     print_slot_machine(slots)
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f'You won ${winnings}')
-    print(f'You on on lines', *winning_lines)
+    print(f'You on lines', *winning_lines)
     return winnings - total_bet
 
 def main():
