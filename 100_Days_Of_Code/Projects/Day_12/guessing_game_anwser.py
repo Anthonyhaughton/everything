a1be9e6185916f2b317f4b29c68d3c930885abad
Haughton <ahaughton@corp.arete.com>
2023-01-04 10:26:29 -0500

    finished game v2

--- 100_Days_Of_Code/Projects/Day_12/guessing_game_anwser.py
+++ 100_Days_Of_Code/Projects/Day_12/guessing_game_anwser.py
@@ -1,14 +1,19 @@
 from random import randint
+from art import logo
+
 # Global Cost to set level tries
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
 # Func to check usr guess agaisnt anwser
-def check_answer(guess, answer):
+def check_answer(guess, answer, turns):
+    ''' Checks answer against guess. Returns the number of turns remaining. '''
     if guess > answer:
         print('Too high!')
+        return turns - 1
     elif guess < answer:
         print('Too low!')
+        return turns - 1
     else:
         print(f'You got it! The answer was {answer}, you win!')
 
@@ -19,19 +24,28 @@ def set_difficulty():
         return EASY_LEVEL_TURNS
     else:
         return HARD_LEVEL_TURNS
-        
+
+# Create a func to run the game      
+def game():
+    #  Choosing a number between  1-100
+    print(logo)
+    answer = randint(1, 100)
+    print('Welcome to the Number Guessing Game!')
+    print("I'm thinking of a number between 1 and 100.")
+    turns = set_difficulty()
     
-#  Choose number between  1-100
-answer = randint(1, 100)
-print('Welcome to the Number Guessing Game!')
-print("I'm thinking of a number between 1 and 100.")
-turns = set_difficulty()
-print(f'You have {turns} attempts remaining to guess the number.')
 
-# Let user guess a number
-guess = input('Make a guess: ')
-check_answer(guess, answer)
+    # Repeat the guessing game if they get it wrong
+    guess = 0 
+    while guess != answer:
+        print(f'You have {turns} attempts remaining to guess the number.')
+        
+        # Let user guess a number
+        guess = int(input('Make a guess: '))
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print(f'You have run out of guesses, you lose! The number was {answer}')
+            return
 
-
-# Track num of turns and sub by 1  if they get it wrong
+game()
 
