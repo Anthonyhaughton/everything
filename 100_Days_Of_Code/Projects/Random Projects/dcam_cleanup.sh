6c6b2464b1fe4e0ccdcd56dd0e1df5ba1e8e0daa
Haughton <ahaughton@corp.arete.com>
2023-04-03 16:37:08 -0400

    added linux scripts

---
+++ 100_Days_Of_Code/Projects/Random Projects/dcam_cleanup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+menu ="100"
+
+while [ menu != "10" ]
+do
+
+	echo ""
+	echo ""
+	echo "DCAM Project Folder Helper"
+	echo "Written by Anthony Haughton"
+	echo "Notes: Script must be run as root"
+	echo "-------------------------------------------"
+	echo "1.  Make Folder"
+	echo "10. Quit"
+	echo "-------------------------------------------"
+	read -p "Enter your selection: " menu
+
+	if [ $menu = "1" ]; then
+
+		# Get folder name
+
+		echo -n "What folder do you want to create? "
+		read -r folder
+
+		# Ask if they know the group for the folder
+
+		echo -n "Do you know the AD group for this folder? y/n: "
+		read -r ask_group
+
+		# Get machine name
+
+		machine=$(hostname | tr '[:upper:]' '[:lower:]')
+
+		# Create folder and set permissions on slow drive
+
+		mkdir /mnt/$machine/slow01/$folder
+		chown :root /mnt/$machine/slow01/$folder
+		chmod 2770 /mnt/$machine/slow01/$folder
+
+		# Ask if they want to create the same folders on the fast drive
+
+		echo "Do you want to make the same folder on the fast drive? y/n: "
+		read -r fast
+
+		if [ $fast = y ]; then
+			mkdir /mnt/$machine/fast01/$folder
+			chown :root /mnt/$machine/fast01/$folder
+			chmod 2770 /mnt/$machine/fast01/$folder
+		fi
+
+		if [ $ask_group = y ]; then
+				echo -n "What's the group? "
+				read -r group
+			chgrp $group /mnt/$machine/slow01/$folder
+			chgrp $group /mnt/$machine/fast01/$folder
+		fi
+		
+		echo ""
+		echo "The $folder folder has been created on $machine"
+		sleep 5
+	
+
+	elif [ $menu = "10" ]; then
+		exit
+	fi
+
+done
