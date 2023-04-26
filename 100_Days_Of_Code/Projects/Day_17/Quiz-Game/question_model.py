06b9f0360398c4438e3a0d4f60942c14aa23bd8c
Haughton <ahaughton@corp.arete.com>
2023-04-26 14:07:31 -0400

    finished quiz project

--- 100_Days_Of_Code/Projects/Day_17/Quiz-Game/question_model.py
+++ 100_Days_Of_Code/Projects/Day_17/Quiz-Game/question_model.py
@@ -1,6 +1,6 @@
 class Question:
     
-    def __init__(self, text, anwser):
+    def __init__(self, text, answer):
         self.text = text
-        self.anwser = anwser
+        self.answer = answer
         
--- 100_Days_Of_Code/Projects/Day_17/Quiz-Game/quiz_brain.py
+++ 100_Days_Of_Code/Projects/Day_17/Quiz-Game/quiz_brain.py
@@ -2,8 +2,9 @@ class QuizBrain:
     
     def __init__(self, q_list):
         self.question_number = 0 
-        self.question_list = q_list
         self.score = 0
+        self.question_list = q_list
+        
         
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
@@ -16,10 +17,10 @@ class QuizBrain:
         
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
+            self.score += 1
             print('You got it right!')
-            self.score =+ 1
         else:
             print('You got it wrong!')
-    print(f'The correct answer was {correct_answer}')
-    print(f'Your current score is: {self.score}/{self.question_number}')
-    print('/n')
+        print(f'The correct answer was {correct_answer}')
+        print(f'Your current score is: {self.score}/{self.question_number}')
+        print('\n')
