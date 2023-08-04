0130eeb8d43b6a7b1afad2bb0d274e39e1456a3c
Haughton <ahaughton@corp.arete.com>
2023-08-04 18:30:16 -0400

    cleaned up

--- My Projects/refresh_keytab.sh
+++ My Projects/refresh_keytab.sh
@@ -8,19 +8,20 @@ machine=$(hostname)
 
 echo ""
 echo ""
-echo "Keytab Refresh tool"	
+echo "Keytab Refresh Tool"	
 echo "Written by Anthony Haughton"
 echo "Notes: Script must be run as root. Please make sure to delete the PC from AD when prompted"
 
 echo "The current keytab and realm is:"
 ls -lh /etc/krb5.keytab
+sleep 2
 
 # Blank space 
 echo ""
 echo ""
 
-
-echo "Making a backup of the sssd.conf file: "
+# Back up conf file and show backup file
+echo "Making a backup of the sssd.conf file see below: "
 cp /etc/sssd/sssd.conf /etc/sssd/sssd.conf.bak
 ls -lh /etc/sssd/*.bak
 sleep 3
@@ -29,15 +30,15 @@ sleep 3
 echo ""
 echo ""
 
-
-echo "Leaving realm"
+# Leave the realm
+echo "Leaving realm.."
 realm leave
 echo ""
 echo ""
 
 while :
 do
-
+    # Induce infinite loop to make sure user has removed machine from AD
     read -p "Did you remove $machine from the domain? You must do this before you continue.. (y/n): " domain_q
 
     if [ "$domain_q" = "y" ]; then
@@ -48,10 +49,10 @@ do
 
         # Join the realm
         realm join "$realm" -U "$username"
+        echo ""
         echo "welcome to $realm!"
         mv /etc/sssd/sssd.conf.bak /etc/sssd/sssd.conf
         systemctl restart sssd
-        realm list
         sleep 1
 
         echo "Keytab should be updated to todays date: $todaysDate"
@@ -60,7 +61,7 @@ do
         break
 
     else
-        echo "You must remove the machine from AD."
+        echo "You must remove the machine from AD before proceeding."
         sleep 2
     fi
 done
