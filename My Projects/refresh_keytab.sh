747609740c75bff0589ca38dc5f60f4bcd6883b8
Haughton <ahaughton@corp.arete.com>
2023-08-04 18:20:04 -0400

    updated keytab

--- My Projects/refresh_keytab.sh
+++ My Projects/refresh_keytab.sh
@@ -4,6 +4,7 @@ clear
 
 realm=corp.arete.com
 todaysDate=$(date +%F)
+machine=$(hostname)
 
 echo ""
 echo ""
@@ -13,30 +14,53 @@ echo "Notes: Script must be run as root. Please make sure to delete the PC from 
 
 echo "The current keytab and realm is:"
 ls -lh /etc/krb5.keytab
-realm list
-sleep 2
 
-echo "Making a backup of the sssd.conf file"
+# Blank space 
+echo ""
+echo ""
+
+
+echo "Making a backup of the sssd.conf file: "
 cp /etc/sssd/sssd.conf /etc/sssd/sssd.conf.bak
-ls -lh /etc/sssd/
-sleep 2
-
-echo "Leaving realm and rejoining"
-realm leave
-sleep 1
-
-# Prompt for username 
-echo -n "Enter the username: "
-read -r username
-
-# Join the realm
-realm join "$realm" -U "$username"
-echo "welcome to $realm!"
-mv /etc/sssd/sssd.conf.bak /etc/sssd/sssd.conf
-systemctl restart sssd
-realm list
-sleep 1
-
-echo "Keytab should be updated to todays date: $todaysDate"
-ls -lh /etc/krb5.keytab
+ls -lh /etc/sssd/*.bak
 sleep 3
+
+# Blank space 
+echo ""
+echo ""
+
+
+echo "Leaving realm"
+realm leave
+echo ""
+echo ""
+
+while :
+do
+
+    read -p "Did you remove $machine from the domain? You must do this before you continue.. (y/n): " domain_q
+
+    if [ "$domain_q" = "y" ]; then
+        sleep 1
+
+        # Prompt for username to rejoin realm
+        read -p "Enter the username: " username
+
+        # Join the realm
+        realm join "$realm" -U "$username"
+        echo "welcome to $realm!"
+        mv /etc/sssd/sssd.conf.bak /etc/sssd/sssd.conf
+        systemctl restart sssd
+        realm list
+        sleep 1
+
+        echo "Keytab should be updated to todays date: $todaysDate"
+        ls -lh /etc/krb5.keytab
+        sleep 3
+        break
+
+    else
+        echo "You must remove the machine from AD."
+        sleep 2
+    fi
+done
