b0f0ad32a116fc5c95d6367e813319a605d2ca0d
Haughton <ahaughton@corp.arete.com>
2023-12-27 17:36:35 -0500

    updated html files

--- IaC/Terraform/Static Site/storage.tf
+++ IaC/Terraform/Static Site/storage.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "s3_site" {
   bucket = "devhaughton.com"
-  
+
   tags = {
      Name        = "My bucket"
      Environment = "Dev"
@@ -17,32 +17,37 @@ resource "aws_s3_object" "file_upload" {
   content_type = each.value
 }
 
-# This block is needed to make the bucket fully public but for some reason I keep getting access denied and it looks
-# like the same is happeneing to others online, Not sure of the fix yet so adding policy in console.
-
-# resource "aws_s3_bucket_policy" "s3_GetObj" {
-#   bucket = aws_s3_bucket.s3_site.id
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Id      = "GetObject"
-#     Statement = [
-#       {
-#         Sid       = "PublicReadGetObject"
-#         Effect    = "Allow"
-#         Principal = "*"
-#         Action    = ["s3:GetObject"]
-#         Resource  = "${aws_s3_bucket.s3_site.arn}/*"
-#       },
-#     ]
-#   })
-# }
-
 resource "aws_s3_bucket_public_access_block" "public_s3" {
   bucket = aws_s3_bucket.s3_site.id
 
   block_public_acls   = false
   block_public_policy = false
+}
+
+# This block is needed to make the bucket fully public but for some reason I keep getting access denied and it looks
+# like the same is happeneing to others online, Not sure of the fix yet so adding policy in console.
+
+resource "aws_s3_bucket_policy" "s3_GetObj" {
+  bucket = aws_s3_bucket.s3_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "GetObject"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = "${aws_s3_bucket.s3_site.arn}/*"
+      },
+    ]
+  })
+
+  depends_on = [ 
+    
+    "aws_s3_bucket_public_access_block.public_s3" # This fixed the issue with the bucket policy getting access denied!
+  ]
 }
 
 resource "aws_s3_bucket_versioning" "versioning_s3" {
